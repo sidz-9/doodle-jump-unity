c7c9f0ad053ece9a202a579558c7c5b44c8fa8c2
sidz-9 <sid.zaveri.dev@gmail.com>
2021-03-20 00:24:21 +0530

    Doodle, background and platform sprites added. Platform script added.

---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    public float jumpSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D col) {
+
+        if(col.relativeVelocity.y <= 0f) {      // coming from top
+            Rigidbody2D rb = col.collider.GetComponent<Rigidbody2D>();
+            if(rb != null) {
+                rb.velocity = new Vector2(0f, jumpSpeed);
+            }
+        }
+    }
+}
