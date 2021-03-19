13a42b82e5956b230f6e2f4f9da62e362fe4c2ca
sidz-9 <sid.zaveri.dev@gmail.com>
2021-03-20 01:29:05 +0530

    Created player script and added horizaontal player movement.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    float movement = 0f;
+
+    public float movementSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxis("Horizontal") * movementSpeed;
+    }
+
+    void FixedUpdate() {
+        rb.velocity = new Vector2(movement, rb.velocity.y);
+    }
+}
